d5cedf62dc2a0ef0fedd5ac8cb41442b65b959cd
OGSegu <seguisreal@gmail.com>
2020-03-10 16:32:34 +0300

    Fixed all test's issues.

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,5 +1,4 @@
 import org.junit.jupiter.api.Test;
-
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -12,30 +11,30 @@ class MainTest {
     void mainTest() throws IOException {
         Flags flag = new Flags();
         flag.setDirectory(new File("testfolder/folder/inner-folder"));
-        assertEquals("FileInformation.java\nFlags.java\nFunctions.java\nMain.java", Functions.printDirectory(flag));
+        assertEquals("FileInformation.test\nFlags.test\nFunctions.test\nMain.test", Functions.printDirectory(flag));
         flag.setDirectory(new File("testfolder/blabla"));
         assertEquals("Данного каталога не существует", Functions.printDirectory(flag));
-        flag.setDirectory(new File("testfolder/folder/inner-folder/Main.java"));
-        assertEquals("111 10.03.2020 15:19:53 601 Main.java", Functions.printDirectory(flag));
+        flag.setDirectory(new File("testfolder/folder/inner-folder/Main.test"));
+        assertEquals("111 10.03.2020 15:19:53 601 Main.test", Functions.printDirectory(flag));
         flag.setLFlag(true);
         flag.setDirectory(new File("testfolder/folder"));
-        assertEquals("111 10.03.2020 15:25:07 2221 Flags.java\n111 10.03.2020 15:33:48 6755 inner-folder",
+        assertEquals("111 10.03.2020 15:25:07 2221 Flags.test\n111 10.03.2020 16:29:30 6755 inner-folder",
                 Functions.printDirectory(flag));
         flag.setHFlag(true);
-        assertEquals("rwx 10.03.2020 15:25:07 2,2К Flags.java\nrwx 10.03.2020 15:33:48 6,6К inner-folder",
+        assertEquals("rwx 10.03.2020 15:25:07 2,2К Flags.test\nrwx 10.03.2020 16:29:30 6,6К inner-folder",
                 Functions.printDirectory(flag));
         flag.setRFlag(true);
-        assertEquals("rwx 10.03.2020 15:33:48 6,6К inner-folder\nrwx 10.03.2020 15:25:07 2,2К Flags.java",
+        assertEquals("rwx 10.03.2020 16:29:30 6,6К inner-folder\nrwx 10.03.2020 15:25:07 2,2К Flags.test",
                 Functions.printDirectory(flag));
         Main.main(new String[]{"-o", "test.txt", "testfolder/folder/inner-folder"});
-        assertEquals("FileInformation.java\nFlags.java\nFunctions.java\nMain.java",
+        assertEquals("FileInformation.test\nFlags.test\nFunctions.test\nMain.test",
                 new String(Files.readAllBytes(Paths.get("test.txt"))));
         assertThrows(IllegalArgumentException.class, () -> Main.main(new String[]{"-o"}));
         Main.main(new String[]{"-l", "-h", "-r", "-o", "test.txt", "testfolder/folder/inner-folder"});
-        assertEquals("rwx 10.03.2020 15:19:53 601,0Б Main.java\n" +
-                        "rwx 10.03.2020 15:30:39 1,5К Functions.java\n" +
-                        "rwx 10.03.2020 15:25:07 2,2К Flags.java\n" +
-                        "rwx 10.03.2020 14:18:39 2,3К FileInformation.java",
+        assertEquals("rwx 10.03.2020 15:19:53 601,0Б Main.test\n" +
+                        "rwx 10.03.2020 15:30:39 1,5К Functions.test\n" +
+                        "rwx 10.03.2020 15:25:07 2,2К Flags.test\n" +
+                        "rwx 10.03.2020 14:18:39 2,3К FileInformation.test",
                 new String(Files.readAllBytes(Paths.get("test.txt"))));
     }
 }
