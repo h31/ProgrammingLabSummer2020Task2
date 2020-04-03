1d1f9d5e8eaf4e624fcccbee193d0d47af6032bd
galievaregina <regina-galieva-1996@mail.ru>
2020-04-03 22:20:35 +0300

    du

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -7,38 +7,38 @@ class MainTest {
     @Test
     void main() throws IOException {
         Main.main(new String[]{"-h", "-si", "-c", "src/test/resources/cat.txt", "src/test/resources"});
-        assertEquals("5,1K src\\test\\resources\\cat.txt\r\n" +
-                "12,0K src\\test\\resources\r\n" +
-                "17,1K", new String(Files.readAllBytes(Paths.get("output"))));
+        assertEquals("5,0K src\\test\\resources\\cat.txt\r\n" +
+                "8,8K src\\test\\resources\r\n" +
+                "13,8K", new String(Files.readAllBytes(Paths.get("output"))));
         Main.main(new String[]{"-si","-c","src/test/resources/cat.txt","src/test/resources"});
-        assertEquals("5,1K src\\test\\resources\\cat.txt\r\n" +
-                "12,0K src\\test\\resources\r\n" +
-                "17,1K",new String(Files.readAllBytes(Paths.get("output"))));
+        assertEquals("5,0K src\\test\\resources\\cat.txt\r\n" +
+                "8,8K src\\test\\resources\r\n" +
+                "13,8K",new String(Files.readAllBytes(Paths.get("output"))));
         Main.main(new String[]{"-c","src/test/resources/cat.txt","src/test/resources"});
         assertEquals("4,9K src\\test\\resources\\cat.txt\r\n" +
-                "11,7K src\\test\\resources\r\n" +
-                "16,7K", new String(Files.readAllBytes(Paths.get("output"))));
+                "8,6K src\\test\\resources\r\n" +
+                "13,5K", new String(Files.readAllBytes(Paths.get("output"))));
         Main.main(new String[]{"-h","-c","src/test/resources/cat.txt","src/test/resources"});
         assertEquals("4,9K src\\test\\resources\\cat.txt\r\n" +
-                "11,7K src\\test\\resources\r\n" +
-                "16,7K",new String(Files.readAllBytes(Paths.get("output"))));
+                "8,6K src\\test\\resources\r\n" +
+                "13,5K",new String(Files.readAllBytes(Paths.get("output"))));
         Main.main(new String[]{"-h","-si","src/test/resources/cat.txt","src/test/resources"});
-        assertEquals("5,1K src\\test\\resources\\cat.txt\r\n" +
-                "12,0K src\\test\\resources",
+        assertEquals("5,0K src\\test\\resources\\cat.txt\r\n" +
+                "8,8K src\\test\\resources",
                 new String(Files.readAllBytes(Paths.get("output"))));
         Main.main(new String[]{"-h","src/test/resources/cat.txt","src/test/resources","src/regina"});
         assertEquals("4,9K src\\test\\resources\\cat.txt\r\n" +
-                "11,7K src\\test\\resources\r\n" +
+                "8,6K src\\test\\resources\r\n" +
                 "Данного файла или каталога не существует",new String(Files.readAllBytes(Paths.get("output"))));
         Main.main(new String[]{"-si","src/test/resources/cat.txt","src/test/resources","src/test/resources/dog.txt"});
-        assertEquals("5,1K src\\test\\resources\\cat.txt\r\n" +
-                "12,0K src\\test\\resources\r\n" +
-                "5,1K src\\test\\resources\\dog.txt",
+        assertEquals("5,0K src\\test\\resources\\cat.txt\r\n" +
+                "8,8K src\\test\\resources\r\n" +
+                "3,2K src\\test\\resources\\dog.txt",
                 new String(Files.readAllBytes(Paths.get("output"))));
         Main.main(new String[]{"src/test/resources/cat.txt","src/test/resources","src/test/resources/dog.txt"});
         assertEquals("4,9K src\\test\\resources\\cat.txt\r\n" +
-                "11,7K src\\test\\resources\r\n" +
-                "5,0K src\\test\\resources\\dog.txt",new String(Files.readAllBytes(Paths.get("output"))));
+                "8,6K src\\test\\resources\r\n" +
+                "3,1K src\\test\\resources\\dog.txt",new String(Files.readAllBytes(Paths.get("output"))));
 
 
 
