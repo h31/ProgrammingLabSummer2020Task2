f1c3263b1c66940b57fb0b51db67cb8ca02fedf3
KoTTiG <d.gasilov@mail.ru>
2020-04-09 01:57:58 +0300

    test commit.2

--- src/test/java/du/FileWeightTest.java
+++ src/test/java/du/FileWeightTest.java
@@ -15,34 +15,39 @@ class FileWeightTest {
 
     @Test
     void h() throws IOException {
-        assertEquals("100.4KB ", createFileWeight(new String[]{"-h", "src/test/resources"}));
-        assertEquals("98.3KB ", createFileWeight(new String[]{"-h", "src/test/resources/test3.txt"}));
-        assertEquals("110.0B 2.0KB 100.4KB ", createFileWeight(new String[]{"-h", "src/test/resources/test.txt", "src/test/resources/test2.txt", "src/test/resources"}));
+        assertEquals("279.7KB ", createFileWeight(new String[]{"-h", "src/test/resources"}));
+        assertEquals("277.6KB ", createFileWeight(new String[]{"-h", "src/test/resources/file3.txt"}));
+        assertEquals("279.7KB 101.0B 1.9KB ", createFileWeight(new String[]{"-h", "src/test/resources", "src/test/resources/file1.txt", "src/test/resources/file2.txt"}));
     }
 
     @Test
     void si() throws IOException {
-        assertEquals("102.8 ", createFileWeight(new String[]{"-si", "src/test/resources"}));
-        assertEquals("100.7 ", createFileWeight(new String[]{"-si", "src/test/resources/test3.txt"}));
-        assertEquals("0.1 2.0 102.8 ", createFileWeight(new String[]{"-si", "src/test/resources/test.txt", "src/test/resources/test2.txt", "src/test/resources"}));
+        assertEquals("286.4 ", createFileWeight(new String[]{"-si", "src/test/resources"}));
+        assertEquals("284.3 ", createFileWeight(new String[]{"-si", "src/test/resources/file3.txt"}));
+        assertEquals("286.4 0.1 2.0 ", createFileWeight(new String[]{"-si", "src/test/resources", "src/test/resources/file1.txt", "src/test/resources/file2.txt"}));
     }
 
     @Test
     void hSi() throws IOException {
-        assertEquals("102.8KB ", createFileWeight(new String[]{"-si", "-h", "src/test/resources"}));
-        assertEquals("100.7KB ", createFileWeight(new String[]{"-si", "-h", "src/test/resources/test3.txt"}));
-        assertEquals("110.0B 2.0KB 102.8KB ", createFileWeight(new String[]{"-si","-h", "src/test/resources/test.txt", "src/test/resources/test2.txt", "src/test/resources"}));
+        assertEquals("286.4KB ", createFileWeight(new String[]{"-si", "-h", "src/test/resources"}));
+        assertEquals("284.3KB ", createFileWeight(new String[]{"-si", "-h", "src/test/resources/file3.txt"}));
+        assertEquals("286.4KB 101.0B 2.0KB ", createFileWeight(new String[]{"-si","-h", "src/test/resources", "src/test/resources/file1.txt", "src/test/resources/file2.txt"}));
     }
 
     @Test
     void noFlag() throws IOException {
-        assertEquals("100.4 ", createFileWeight(new String[]{"src/test/resources"}));
-        assertEquals("98.3 ", createFileWeight(new String[]{"src/test/resources/test3.txt"}));
-        assertEquals("0.1 2.0 100.4 ", createFileWeight(new String[]{"src/test/resources/test.txt", "src/test/resources/test2.txt", "src/test/resources"}));
+        assertEquals("279.7 ", createFileWeight(new String[]{"src/test/resources"}));
+        assertEquals("277.6 ", createFileWeight(new String[]{"src/test/resources/file3.txt"}));
+        assertEquals("279.7 0.1 1.9 ", createFileWeight(new String[]{"src/test/resources","src/test/resources/file1.txt", "src/test/resources/file2.txt"}));
     }
 
     @Test
     void c() throws IOException {
-        assertEquals("102.5", createFileWeight(new String[]{"-c", "src/test/resources/test.txt", "src/test/resources/test2.txt", "src/test/resources"}));
+        assertEquals("279.7", createFileWeight(new String[]{"-c", "src/test/resources/file3.txt", "src/test/resources/file1.txt", "src/test/resources/file2.txt"}));
+    }
+
+    @Test
+    void ch() throws IOException {
+        assertEquals("2.0KB", createFileWeight(new String[]{"-c", "-h","src/test/resources/file1.txt", "src/test/resources/file2.txt"}));
     }
 }
