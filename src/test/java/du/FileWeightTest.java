ffe9df92e48dd58d81def8daa4eaa92b93339359
KoTTiG <d.gasilov@mail.ru>
2020-04-09 01:03:51 +0300

    test commit

--- src/test/java/du/FileWeightTest.java
+++ src/test/java/du/FileWeightTest.java
@@ -15,39 +15,34 @@ class FileWeightTest {
 
     @Test
     void h() throws IOException {
-        assertEquals("40.4MB ", createFileWeight(new String[]{"-h", "src/test/resources"}));
-        assertEquals("5.0B ", createFileWeight(new String[]{"-h", "src/test/resources/test3.txt"}));
-        assertEquals("110.0B 98.3KB 40.4MB ", createFileWeight(new String[]{"-h", "src/test/resources/test.txt", "src/test/resources/testDir/test1.txt", "src/test/resources"}));
+        assertEquals("100.4KB ", createFileWeight(new String[]{"-h", "src/test/resources"}));
+        assertEquals("98.3KB ", createFileWeight(new String[]{"-h", "src/test/resources/test3.txt"}));
+        assertEquals("110.0B 2.0KB 100.4KB ", createFileWeight(new String[]{"-h", "src/test/resources/test.txt", "src/test/resources/test2.txt", "src/test/resources"}));
     }
 
     @Test
     void si() throws IOException {
-        assertEquals("42371.1 ", createFileWeight(new String[]{"-si", "src/test/resources"}));
-        assertEquals("0.0 ", createFileWeight(new String[]{"-si", "src/test/resources/test3.txt"}));
-        assertEquals("0.1 100.7 42371.1 ", createFileWeight(new String[]{"-si", "src/test/resources/test.txt", "src/test/resources/testDir/test1.txt", "src/test/resources"}));
+        assertEquals("102.8 ", createFileWeight(new String[]{"-si", "src/test/resources"}));
+        assertEquals("100.7 ", createFileWeight(new String[]{"-si", "src/test/resources/test3.txt"}));
+        assertEquals("0.1 2.0 102.8 ", createFileWeight(new String[]{"-si", "src/test/resources/test.txt", "src/test/resources/test2.txt", "src/test/resources"}));
     }
 
     @Test
     void hSi() throws IOException {
-        assertEquals("42.4MB ", createFileWeight(new String[]{"-si", "-h", "src/test/resources"}));
-        assertEquals("5.0B ", createFileWeight(new String[]{"-si", "-h", "src/test/resources/test3.txt"}));
-        assertEquals("110.0B 100.7KB 42.4MB ", createFileWeight(new String[]{"-si","-h", "src/test/resources/test.txt", "src/test/resources/testDir/test1.txt", "src/test/resources"}));
+        assertEquals("102.8KB ", createFileWeight(new String[]{"-si", "-h", "src/test/resources"}));
+        assertEquals("100.7KB ", createFileWeight(new String[]{"-si", "-h", "src/test/resources/test3.txt"}));
+        assertEquals("110.0B 2.0KB 102.8KB ", createFileWeight(new String[]{"-si","-h", "src/test/resources/test.txt", "src/test/resources/test2.txt", "src/test/resources"}));
     }
 
     @Test
     void noFlag() throws IOException {
-        assertEquals("41378.0 ", createFileWeight(new String[]{"src/test/resources"}));
-        assertEquals("0.0 ", createFileWeight(new String[]{"src/test/resources/test3.txt"}));
-        assertEquals("0.1 98.3 41378.0 ", createFileWeight(new String[]{"src/test/resources/test.txt", "src/test/resources/testDir/test1.txt", "src/test/resources"}));
+        assertEquals("100.4 ", createFileWeight(new String[]{"src/test/resources"}));
+        assertEquals("98.3 ", createFileWeight(new String[]{"src/test/resources/test3.txt"}));
+        assertEquals("0.1 2.0 100.4 ", createFileWeight(new String[]{"src/test/resources/test.txt", "src/test/resources/test2.txt", "src/test/resources"}));
     }
 
     @Test
     void c() throws IOException {
-        assertEquals("41376.1", createFileWeight(new String[]{"-c", "src/test/resources/test.txt", "src/test/resources/testDir/test1.txt", "src/test/resources/test3.txt", "src/test/resources/test2.txt"}));
+        assertEquals("102.5", createFileWeight(new String[]{"-c", "src/test/resources/test.txt", "src/test/resources/test2.txt", "src/test/resources"}));
     }
-
-    @Test
-    void hCSi() throws IOException {
-        assertEquals("42.4MB", createFileWeight(new String[]{"-si", "-h", "-c", "src/test/resources/test.txt", "src/test/resources/testDir/test1.txt", "src/test/resources/test3.txt", "src/test/resources/test2.txt"}));
-        }
 }
