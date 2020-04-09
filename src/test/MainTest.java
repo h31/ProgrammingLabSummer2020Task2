9e4cf66c26498fd925b005090183cbc8a6a1df19
shomov <mikle@shomov.spb.ru>
2020-04-09 23:38:49 +0300

    Add deleteFolderAndItsContent

--- src/test/MainTest.java
+++ src/test/MainTest.java
@@ -1,26 +1,31 @@
 import org.junit.jupiter.api.Test;
 import java.io.*;
 import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
     @Test
     void main() throws IOException {
+        Files.createDirectory(Paths.get("testDirectory/folder"));
         Path file = Paths.get("testDirectory/test.txt");
+        Files.copy(file, Paths.get("testDirectory/folder/test.txt"));
+        file = Paths.get("testDirectory/folder/test.txt");
         String key1 = "123ABC";
         String key2 = "HHFLE7";
         String key3 = "FE54A4D";
-        Path fileEncode = Paths.get("testDirectory/test-encode.txt");
-        Path fileDecode = Paths.get("testDirectory/test.txt.crp");
-        Path fileEDE = Paths.get("testDirectory/test-encode-2.txt.crp");
-        Path fileEDED = Paths.get("testDirectory/test-encode-2.txt");
+        Path fileEncode = Paths.get("testDirectory/folder/test-encode.txt");
+        Path fileDecode = Paths.get("testDirectory/folder/test.txt.crp");
+        Path fileEDE = Paths.get("testDirectory/folder/test-encode-2.txt.crp");
+        Path fileEDED = Paths.get("testDirectory/folder/test-encode-2.txt");
 
         Main.main(new String[]{"-c", key1, file.toString()});
         Main.main(new String[]{"-d", key1, fileDecode.toString(), "-o", fileEncode.toString()});
         Main.main(new String[]{"-c", key1, fileEncode.toString()});
         Main.main(new String[]{"-c", key3, fileEncode.toString(), "-o", fileEDE.toString()});
         Main.main(new String[]{"-d", key1, fileEDE.toString()});
-        Path fileEncodeDecode = Paths.get("testDirectory/test-encode.txt.crp");
+        Path fileEncodeDecode = Paths.get("testDirectory/folder/test-encode.txt.crp");
 
         {
             assertTrue(textEquals(file, fileEncode));
@@ -30,18 +35,14 @@ class MainTest {
             assertFalse(textEquals(fileEncode, fileEDED));
             assertFalse(textEquals(file, fileEDED));
 
-            assertThrows(IllegalArgumentException.class, () -> Main.main(new String[]{"-c", key2, file.toString()}));
-            assertThrows(FileNotFoundException.class, () -> Main.main(new String[]{"-c", key1, "testDirectory/test-fail.txt"}));
-            assertThrows(FileAlreadyExistsException.class, () -> Main.main(new String[]{"-c", key1, "testDirectory/test.txt"}));
+            Path finalFile = file;
+            assertThrows(IllegalArgumentException.class, () -> Main.main(new String[]{"-c", key2, finalFile.toString()}));
+            assertThrows(FileNotFoundException.class, () -> Main.main(new String[]{"-c", key1, "testDirectory/folder/test-fail.txt"}));
+            assertThrows(FileAlreadyExistsException.class, () -> Main.main(new String[]{"-c", key1, "testDirectory/folder/test.txt"}));
             assertThrows(AccessDeniedException.class, () -> Main.main(new String[]{"-c", key1, "/run/systemd/generator/mnt-sda1.mount"}));
-            assertThrows(FileAlreadyExistsException.class, () -> Main.main(new String[]{"-c", key1, "testDirectory/test.txt", "-o", "testDirectory/test.txt"}));
+            assertThrows(FileAlreadyExistsException.class, () -> Main.main(new String[]{"-c", key1, "testDirectory/folder/test.txt", "-o", "testDirectory/folder/test.txt"}));
         }
 
-        Files.delete(fileDecode);
-        Files.delete(fileEncode);
-        Files.delete(fileEncodeDecode);
-        Files.delete(fileEDE);
-        Files.delete(fileEDED);
     }
 
     boolean textEquals (Path file1, Path file2) throws IOException {
@@ -59,4 +60,23 @@ class MainTest {
         reader2.close();
         return equal;
     }
+
+    @Test
+    void deleteFolderAndItsContent() throws IOException {
+        Path folder = Paths.get("testDirectory/folder");
+        Files.walkFileTree(folder, new SimpleFileVisitor<>() {
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                Files.delete(file);
+                return FileVisitResult.CONTINUE;
+            }
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                if (exc != null) {
+                    throw exc;
+                }
+                Files.delete(dir);
+                return FileVisitResult.CONTINUE;
+            }
+        });
+    }
+    //The idea is borrowed from https://stackoverflow.com/questions/20281835/how-to-delete-a-folder-with-files-using-java
 }
