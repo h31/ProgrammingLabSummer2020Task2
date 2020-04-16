1fc9b04a892bd92faa1bcb491d30e1297b3e6f93
galievaregina <regina-galieva-1996@mail.ru>
2020-04-16 19:03:09 +0300

    du

--- src/test/java/SizeTest.java
+++ src/test/java/SizeTest.java
@@ -7,19 +7,18 @@ import java.io.IOException;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SizeTest {
-    File file = new File("regina.txt");
-    FileWriter writer = new FileWriter(file);
+    SizeTest() throws IOException {
+    }
 
     File writeText() throws IOException {
+        File file = new File("regina.txt");
+        FileWriter writer = new FileWriter(file);
         writer.write("i you we wish you gedree jgjfhwh 99993200123984849020339urh ffner");
         writer.close();
         return file;
     }
 
-    Size simple = new Size(file);
-
-    SizeTest() throws IOException {
-    }
+    Size simple = new Size(writeText());
 
     @Test
     void getName() {
