d671194ea5e7b40208337353d5e356438bad04c7
Ilya <ilya.orlov2002@yandex.ru>
2020-04-19 19:25:12 +0400

    Теперь jar собран нормально

--- src/test/java/com/example/project/TailerLauncherTest.java
+++ src/test/java/com/example/project/TailerLauncherTest.java
@@ -10,33 +10,36 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class TailerLauncherTest {
     boolean equalToOutput(String expected) throws IOException {
-        return Files.readAllLines(Paths.get("test_files/output.txt")).equals(Files.readAllLines(Paths.get(expected)));
+        return Files.readAllLines(Paths.get("src/test/test_files/output.txt")).equals(Files.readAllLines(Paths.get(expected)));
     }
 
     @Test
     void testLastSymbols() throws IOException {
-        String[] args = {"-c", "5", "-o", "test_files/output.txt", "test_files/input.txt", "test_files/input1.txt"};
+        String[] args = {"-c", "5", "-o", "src/test/test_files/output.txt",
+                "src/test/test_files/input.txt", "src/test/test_files/input1.txt"};
         TailerLauncher.main(args);
-        assertTrue(equalToOutput("test_files/expectedSym.txt"));
+        assertTrue(equalToOutput("src/test/test_files/expectedSym.txt"));
     }
 
     @Test
     void testLast5Strings () throws IOException {
-        String[] args = {"-n", "5", "-o", "test_files/output.txt", "test_files/input.txt", "test_files/input1.txt"};
+        String[] args = {"-n", "5", "-o", "src/test/test_files/output.txt",
+                "src/test/test_files/input.txt", "src/test/test_files/input1.txt"};
         TailerLauncher.main(args);
-        assertTrue(equalToOutput("test_files/expected5Str.txt"));
+        assertTrue(equalToOutput("src/test/test_files/expected5Str.txt"));
     }
 
     @Test
     void testLast10Strings () throws IOException {
-        String[] args = {"-o", "test_files/output.txt", "test_files/input.txt", "test_files/input1.txt"};
+        String[] args = {"-o", "src/test/test_files/output.txt", "src/test/test_files/input.txt",
+                "src/test/test_files/input1.txt"};
         TailerLauncher.main(args);
-        assertTrue(equalToOutput("test_files/expected10Str.txt"));
+        assertTrue(equalToOutput("src/test/test_files/expected10Str.txt"));
     }
 
     @Test
     void testBothFlags () {
-        String[] args = {"-n", "3", "-c", "5", "test_files/input.txt"};
+        String[] args = {"-n", "3", "-c", "5", "src/test/test_files/input.txt"};
         assertThrows(Error.class, () -> TailerLauncher.main(args));
     }
 }
